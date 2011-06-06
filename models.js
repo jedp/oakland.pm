8cafb2f6eb29326a6d51ff64c3154ed6b325ef41
Jed Parsons <jedp@me.com>
2011-06-06 00:13:31 -0700

    can now bootstrap from city csv data

--- models.js
+++ models.js
@@ -1,38 +1,147 @@
-//var mongoose = require('mongoose/');
-//mongoose.connect ('mongodb://localhost/' + settings.dbname);
+var mongoose = require('mongoose/');
+mongoose.connect ('mongodb://localhost/oakland-pm');
 
-exports.events = [
-  { id: 123,
-    who: "Northumbrian Quilting Consortium",
-    about: "Here at Urban Strategies Council we’re lucky to work with a wide range of partner organizations throughout the city and county in government agencies, community based organizations, resident groups, organizing groups and faith based communities. ",
-    where: "300 Lakeside Drive, Oakland",
-    title: "Quilting Club",
-    date: 'in 6 hours',
-    tags: ['Crafts'],
-    distance: '1.6mi',
-    attending: [
-      "Loretta",
-      "Erasmus",
-      "Pinto",
-      "Juanita",
-      "Herve",
-      "Pertelote" ],
-    seats: 14  } ,
+var Schema = mongoose.Schema;
 
-  { id: 456,
-    title: "Cello Piñata Society",
-    who: "Young Men's Cellists Association",
-    about: "We’ve been asking folks for ideas and issues that are important to them that may be overcome, aided or improved through the use of great technology-based tools, especially mobile apps for use by those without fancy (and expensive) smartphones. The following ideas for apps or tools came from a variety of people working to improve our city for the residents of our city, take a minute to read over these to see if they are of interest to you as a developer seeking great ideas or to see if someone else is possibly thinking about the same issues you are.",
-    where: "300 Lakeside Drive, Oakland",
-    date: 'in 2 days',
-    tags: ['Crafts', 'Music'],
-    distance: '1.8mi',
-    attending: [
-      ],
-    seats: 6 }
-];
+var LocationSchema = new Schema({
+  address: String,
+  bus: String,
+  bart: String,
+  loc: { lat:  { type: Number, min: -90,  max: 90  }, 
+         long: { type: Number, min: -180, max: 180 } }
+});
 
-exports.user = {
-  'username': "sonja",
-  'interests': ['Crafts']
-};
+var ProgramSchema = new Schema({
+  name: String,
+  agency: String,
+  address: String,
+  city: String,
+  state: String,
+  zip: String,
+  district: String,
+  phone: String,
+  fax: String,
+  email: String,
+  web: String,
+  tdd: String,
+
+  address: String,
+  bus: String,
+  bart: String,
+  loc: { lat:  { type: Number, min: -90,  max: 90  }, 
+         long: { type: Number, min: -180, max: 180 } },
+
+  duration: String,
+  description: String,
+  eligibility: String,
+  dates: [Date],
+  agemin: Number,
+  agemax: Number,
+  fees: String, 
+
+  tags: [String],
+  attending: [String],
+});
+
+function starsListToDict(list) {
+  // turn csv data from stars to a dictionary
+  return {
+    ID: list[0],
+    program: list[1],
+    agency: list[2],
+    address: list[3],
+    city: list[4],
+    state: list[5],
+    zip: list[6],
+    district: list[7],
+    phone: list[8],
+    fax: list[9],
+    tdd: list[10],
+    email: list[11],
+    web: list[12],
+    duration: list[13],
+    description: list[14],
+    eligibility: list[15],
+    days: list[16],
+    hours: list[17],
+    ages: list[18],
+    fees: list[19],
+    access: list[20],
+    translation: list[21],
+    computer: list[22],
+    leadership: list[23],
+    leadershipage: list[24],
+    appdeadline: list[25],
+    requirements: list[26],
+    translator: list[27],
+    bus: list[28],
+    locations: list[29],
+    bart: list[30],
+    volunteer: list[31],
+    logo: list[32],
+    minage: list[33],
+    contactemail: list[34],
+    photo: list[35],
+    download: list[36],
+    contact: list[37],
+    contactphone: list[38],
+    password: list[39],
+    user: list[40],
+    approved: list[41],
+    other: list[42],
+    notes: list[43],
+    provider: list[44],
+    mapaddress: list[45],
+    services: list[46] }
+}
+
+mongoose.model('ProgramModel', ProgramSchema);
+
+var Program = mongoose.model('ProgramModel');
+
+bootstrap = function() {
+  var csv = require('csv');
+  csv()
+    .fromPath(__dirname+'/data/stars-data.csv')
+    .transform(function(data){
+      return starsListToDict(data);
+    })
+    .on('data',function(data,index){
+      if (data.ages.match(/18/) && data.program && data.address) {
+        console.log('#'+index + ': ' + data.program);
+        var program = new Program({
+          name: data.program,
+          agency: data.agency,
+          address: data.address,
+          city: data.city,
+          state: data.state,
+          zip: data.zip,
+          district: data.district,
+          phone: data.phone,
+          fax: data.fax,
+          tdd: data.tdd,
+          email: data.email,
+          web: data.web,
+
+          bus: data.bus,
+          bart: data.bart,
+
+          fees: data.fees
+        });
+        program.save();
+      }
+    });
+}
+
+
+
+
+if (typeof exports !== 'undefined') {
+  exports.Program = Program;
+  exports.starsListToDict = starsListToDict;
+  exports.bootstrap = bootstrap;
+}
+
+// node samples/sample.js
+var csv = require('csv');
+
